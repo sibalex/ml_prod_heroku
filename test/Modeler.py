8bcd2baf569dcfc2b594a08d69223a5acf6b36af
Alexander Sibagatov <43387913+sibalex@users.noreply.github.com>
2020-10-18 11:48:22 +0300

    Update Modeler.py

--- test/Modeler.py
+++ test/Modeler.py
@@ -6,18 +6,18 @@ from sklearn.tree import DecisionTreeClassifier
 
 class Modeler:
     def __init__(self):
-        self.df = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
-        try: self.model = joblib.load('models/iris.model')
+        self.df = pd.read_csv('iris.csv')
+        try: self.model = joblib.load('iris.model')
         except: self.model = None 
 
     def fit(self):
         X = self.df.drop('species', axis=1)
         y = self.df['species']
         self.model = DecisionTreeClassifier().fit(X, y)
-        joblib.dump(self.model, 'models/iris.model')
+        joblib.dump(self.model, 'iris.model')
 
     def predict(self, measurement):
-        if not os.path.exists('models/iris.model'):
+        if not os.path.exists('iris.model'):
             raise Exception('Model not trained yet. Call .fit() before making predictions')
         if len(measurement) != 4:
             raise Exception(f'Expected sepal_length, sepal_width, petal_length, petal_width, but got {measurement}')
